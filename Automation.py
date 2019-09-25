1b46ec1ad601dde3aad4df0fac1b159779886667
Justin Khounsombath <jkhounsombath@ku.edu>
2019-09-25 13:08:47 -0500

    Fixed Spacing on main and Executive files

--- Automation.py
+++ Automation.py
@@ -3,30 +3,27 @@ import shutil
 
 def makeMain():
     m= open("main.cpp", "w+")
-    m.write("""
-    #include <iostream>
-    #include "Executive.h"
-    int main()
-    {
+    m.write("""#include <iostream>
+#include "Executive.h"
+int main()
+{
 
 
-    }
-    """)
+}
+""")
     m.close()
     e= open("Executive.h", "w+")
-    e.write("""
-    #ifndef EXECUTIVE_H
-    #define EXECUTIVE_H
+    e.write("""#ifndef EXECUTIVE_H
+#define EXECUTIVE_H
+#include <iostream>
+#include <string>
 
-    #include <iostream>
-    #include <string>
+Executive()
+{
 
-    Executive()
-    {
-
-    };
-    #endif
-    """)
+};
+#endif
+""")
     e.close()
 def makeDirectory():
     lab= raw_input("What do you want the name of the directory to be: ")
